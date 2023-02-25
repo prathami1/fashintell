5e7b1fd370479bfd599ab8e609810797c28b5ed3
Pratham <62070812+prathami1@users.noreply.github.com>
2023-02-25 03:12:38 -0600

    fixing tab shade

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -54,7 +54,7 @@ export default function Example(props) {
                   'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
                   selected
                     ? 'bg-white shadow'
-                    : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+                    : 'text-blue-100 hover:bg-black/[0.12] hover:text-black'
                 )
               }
             >
