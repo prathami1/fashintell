14939dfdb14f5891a0eb31c4c3da15fd4a5dbb4c
Pratham <62070812+prathami1@users.noreply.github.com>
2023-02-24 19:51:59 -0600

    logo + tagline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Fashintell
+      <header className="App-header">    
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Fash<mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">Intell</mark></h1>
+        <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Find your perfect fit.</p>
       </header>
     </div>
   );
