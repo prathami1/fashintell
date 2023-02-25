e348d8cee56b82493869294fcb8eab3676e7a56c
Sahej Singh <sahej3526@gmail.com>
2023-02-25 14:29:50 -0600

    firebase fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import '../App.css';
 import React from 'react';
 import { collection, addDoc, getDocs } from "firebase/firestore";
-import {db} from '../firebase';
+import {db} from '../services/firebase';
 
 //components
 import Heading from '../components/Heading'
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,6 +9,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+});
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
