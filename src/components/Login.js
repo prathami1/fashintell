f422e58490710911222bc6ec44b6cab814759191
Sahej Singh <sahej3526@gmail.com>
2023-02-25 14:08:13 -0600

    Auth impl

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Login.css"
+import { signInWithGoogle } from "./services/firebase";
+export default function Login() {
+  return (
+      <div className="login-buttons">
+        <button className="login-provider-button" onClick={signInWithGoogle}>
+        <img src="https://img.icons8.com/ios-filled/50/000000/google-logo.png" alt="google icon"/>
+        <span> Continue with Google</span>
+       </button>
+      </div>
+  );
+}
